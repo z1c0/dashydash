a9045f8c1cff7424353b9143afb305a07e33e770
z1c0 <wziegler@live.at>
2017-11-07 20:38:56 +0100

    Further improve pir script

--- util/pir.py
+++ util/pir.py
@@ -11,52 +11,55 @@ import secrets
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-dirName  = os.path.dirname(os.path.realpath(__file__))
-handler = RotatingFileHandler(os.path.join(dirName, 'pir.log'), maxBytes = 20 * 1024 * 1024)
+dirName = os.path.dirname(os.path.realpath(__file__))
+handler = RotatingFileHandler(os.path.join(dirName, 'pir.log'), maxBytes=20 * 1024 * 1024)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s : %(message)s')
-handler.setFormatter(formatter);
+handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
 COOL_DOWN = (60 * 5)
 
-lastPrint = lastMove = dt.datetime.now()
+lastPrint = lastAction = dt.datetime.now()
 state = -1
 
 def toggleScreen(on):
   if on:
-    logger.info("SCREEN ON");
+    logger.info("SCREEN ON")
     os.system("service hdmi start")
   else:
-    logger.info("SCREEN OFF");
+    logger.info("SCREEN OFF")
     os.system("service hdmi stop")
   #with open("/sys/class/backlight/rpi_backlight/bl_power", "w") as text_file:
   #  text_file.write(cmd)
-
 
 def getPirState():
   try:
     response = requests.get('http://' + secrets.HUE_BRIDGE + '/api/' + secrets.USER_ID + '/sensors/13')
     json_data = json.loads(response.text)
-    return 1 if json_data['state']['presence'] == True else 0
+    return json_data['state']['presence'] is True
   except:
-    logger.error(sys.exc_info()[0]);
-    return 1
+    logger.error(sys.exc_info()[0])
+    return True
 
+
+logger.info("*** START ***")
 while True:
-  i = getPirState()
   now = dt.datetime.now()
-  if (now - lastPrint).total_seconds() > 20:
-    logger.info("state is " + str(i));
+  pir = getPirState()
+
+  if (now - lastPrint).total_seconds() > 30:
+    logger.info("state - current: " + str(pir) + ", master: " + str(state))
     lastPrint = now
-  if i == 1:
-    lastMove = now
+
+  if pir is True:
+    lastAction = now
     if state != 1:
       state = 1
       toggleScreen(True)
-  elif i == 0 and state != 0 and (now - lastMove).total_seconds() > COOL_DOWN:
+  elif pir is False and (now - lastAction).total_seconds() > COOL_DOWN:
+    lastAction = now
     state = 0
-    dateTime = now
     toggleScreen(False)
 
-  time.sleep(1.0)
+  time.sleep(0.5)
