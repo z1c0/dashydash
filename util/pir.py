1f3b1de32a51c2eda466b3fdd95af21e32522807
z1c0 <wziegler@live.at>
2018-01-25 22:04:40 +0100

    Improved PIR script

--- util/pir.py
+++ util/pir.py
@@ -6,6 +6,7 @@ import json
 import requests
 import logging
 from logging.handlers import RotatingFileHandler
+import traceback
 import secrets
 
 
@@ -19,6 +20,7 @@ logger.addHandler(handler)
 
 
 COOL_DOWN = (60 * 10)
+PIR_URL = 'http://' + secrets.HUE_BRIDGE + '/api/' + secrets.USER_ID + '/sensors/13'
 
 lastPrint = lastAction = dt.datetime.now()
 state = -1
@@ -26,24 +28,28 @@ state = -1
 def toggleScreen(on):
   if on:
     logger.info("SCREEN ON")
-    os.system("service hdmi start")
+    #os.system("service hdmi start")
+    os.system(vcgencmd display_power 1)
   else:
     logger.info("SCREEN OFF")
-    os.system("service hdmi stop")
+    os.system(vcgencmd display_power 0)
+    #os.system("service hdmi stop")
+
   #with open("/sys/class/backlight/rpi_backlight/bl_power", "w") as text_file:
   #  text_file.write(cmd)
 
 def getPirState():
   try:
-    response = requests.get('http://' + secrets.HUE_BRIDGE + '/api/' + secrets.USER_ID + '/sensors/13')
+    response = requests.get(PIR_URL)
     json_data = json.loads(response.text)
     return json_data['state']['presence'] is True
   except:
-    logger.error(sys.exc_info()[0])
+    logger.exception("exception occurred")
     return True
 
 
 logger.info("*** START ***")
+logger.info("PIR URL: " + PIR_URL)
 while True:
   now = dt.datetime.now()
   pir = getPirState()
