04c287ec1abfa8280161bc427c6a19a2f46c1f53
z1c0 <wziegler@live.at>
2018-02-24 23:58:47 +0100

    Giphy's Random endpoint  working again.

--- src/components/modules/timeofday/timeofday.js
+++ src/components/modules/timeofday/timeofday.js
@@ -8,15 +8,11 @@ function getInfos(req, res) {
   const opts = {
     method: 'GET'
   };
-  //console.log(data);
-  const giphyUrl = 'https://api.giphy.com/v1/gifs/search?api_key=NE1oJoC7rIPJSEjIW13oeAtNA3XUT13F&limit=300&offset=0&rating=G&lang=en&q=' + data.tag;
+  const giphyUrl = 'https://api.giphy.com/v1/gifs/random?api_key=NE1oJoC7rIPJSEjIW13oeAtNA3XUT13F&rating=PG&tag=' + data.tag;
   serverFetcher.fetchJson(giphyUrl)
   .then(function(body) {
-    const item = body.data[Math.floor(Math.random() * body.data.length)];
-    //console.log(item);
-    console.log(item.images.original.url);
     res.json({
-      imageUrl : item.images.original.url,
+      imageUrl : body.data.images.original.url,
       text : data.text,
       emoji : data.emoji,
   });
